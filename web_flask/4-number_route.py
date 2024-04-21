783159af83fe0de776872baec33acc1477f34499
erdey <erdeysyoum@gmail.com>
2024-04-21 03:08:45 -0700

    is number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""start flask app"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ landing page """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello2():
+    """display HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_dynamic(text):
+    """Display dynamic text """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def dynamic2(text='is cool'):
+    """ dynamic text"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    if type(n) is 'integer':
+	return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
