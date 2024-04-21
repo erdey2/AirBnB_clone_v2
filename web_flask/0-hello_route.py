3b43e51a266ebd33dc34381971dd29d9480b0bf9
erdey <erdeysyoum@gmail.com>
2024-04-21 00:46:11 -0700

    hello flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 @app.route = "/"
 """Landing page"""
+
+
 def hello():
-	return '<h1>Hello HBNB!'
+    return '<h1>Hello HBNB!'
 
 
 if __name__ = '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
