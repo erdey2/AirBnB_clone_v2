692c9194915fb6bc40185f4cacfd96f590cf8574
erdey <erdeysyoum@gmail.com>
2024-04-21 00:50:39 -0700

    hello flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """start flask app"""
+
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route = "/"
+@app.route = ('/', strict_slashes=False)
 """Landing page"""
 
 
