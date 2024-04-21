e61b60d18c5b2b71b342d4aa99c83b45d5ebe54c
erdey <erdeysyoum@gmail.com>
2024-04-21 03:18:54 -0700

    is number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,10 +34,10 @@ def dynamic2(text='is cool'):
     return 'Python {}'.format(text)
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    if type(n) is int:
-        return '{} is a number'.format(n)
+    n = str(n)
+    return '{} is a number'.format(n)
 
 
 if __name__ == '__main__':
